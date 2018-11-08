1b28b49a6dd8a69edb6f9c002204584d3eb0f582
hanneshdc <hanneshdc@gmail.com>
2018-11-08 22:08:07 +1300

    Adding more validation rules to cover more forms

--- build-scripts/test.ts
+++ build-scripts/test.ts
@@ -31,11 +31,11 @@ const TEST_FILE = 'test-data/qabca-forms.json';
                     continue;
                 }
                 console.log(`Line ${betterError.start.line} to ${betterError.end.line}`);
-                if ((betterError.end.line - betterError.start.line) >= 10) {
+                if ((betterError.end.line - betterError.start.line) >= 35) {
                     continue;
                 }
 
-                const CONTEXT = 2;
+                const CONTEXT = 5;
                 console.log(dataText.slice(betterError.start.line - CONTEXT, betterError.start.line - 1).join('\n'));
                 console.log(colors.yellow(dataText.slice(betterError.start.line - 1, betterError.end.line).join('\n')));
                 console.log(dataText.slice(betterError.end.line, betterError.end.line + CONTEXT).join('\n'));
